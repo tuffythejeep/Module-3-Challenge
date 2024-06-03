2c1035c6dd53dc744a8331a693788e39a5158691
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-02 22:17:06 -0500

    working on random employee function

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const displayAverageSalary = function (employeesArray) {
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
+  const randInt = Math.random() * employeesArray.length;
+  console.log(employeesArray[randInt]);
 };
 
 /*
